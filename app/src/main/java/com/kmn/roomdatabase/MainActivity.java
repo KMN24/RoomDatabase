aa146cd99d557905840eac076398f2aff7eb3161
Mederbek <kanatbekov.meder.00@gmail.com>
2021-03-17 03:05:01 +0600

    created a new app and implemented Room

---
+++ app/src/main/java/com/kmn/roomdatabase/MainActivity.java
@@ -0,0 +1,22 @@
+package com.kmn.roomdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     *  добавить базу данных Room
+     *  вставить данные / извлечь данные
+     *  добавить контент провайдер над Room
+     *
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
